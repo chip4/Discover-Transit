00fa207e28e0460ba8b8bc581b62256bd62b9d1b
Chip Cely <chip4@gatech.edu>
2012-10-10 17:26:35 -0400

    search working and displaying results in listview

--- gen/com/readystatesoftware/mapviewballoons/R.java
+++ gen/com/readystatesoftware/mapviewballoons/R.java
@@ -41,11 +41,10 @@ public final class R {
         public static final int balloon_item_snippet=0x7f050004;
         public static final int balloon_item_title=0x7f050003;
         public static final int balloon_main_layout=0x7f050002;
-        public static final int help=0x7f05000a;
+        public static final int help=0x7f050009;
         public static final int mainlayout=0x7f050006;
         public static final int mapview=0x7f050007;
-        public static final int menu_search=0x7f050009;
-        public static final int searchlayout=0x7f050008;
+        public static final int menu_search=0x7f050008;
     }
     public static final class layout {
         public static final int balloon_overlay=0x7f030000;
