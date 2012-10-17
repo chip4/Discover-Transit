8898e3c3e5e2ed426566cafde9d24514f684e534
Chip Cely <chip4@gatech.edu>
2012-10-17 18:24:12 -0400

    app running, autocomplete broken

--- gen/com/readystatesoftware/mapviewballoons/R.java
+++ gen/com/readystatesoftware/mapviewballoons/R.java
@@ -16,24 +16,23 @@ public final class R {
         public static final int balloon_overlay_close=0x7f020002;
         public static final int balloon_overlay_focused=0x7f020003;
         public static final int balloon_overlay_unfocused=0x7f020004;
-        public static final int black=0x7f020005;
-        public static final int bus=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int icon=0x7f020008;
-        public static final int m1=0x7f020009;
-        public static final int m10=0x7f02000a;
-        public static final int m2=0x7f02000b;
-        public static final int m3=0x7f02000c;
-        public static final int m4=0x7f02000d;
-        public static final int m5=0x7f02000e;
-        public static final int m6=0x7f02000f;
-        public static final int m7=0x7f020010;
-        public static final int m8=0x7f020011;
-        public static final int m9=0x7f020012;
-        public static final int marker=0x7f020013;
-        public static final int marker2=0x7f020014;
-        public static final int mini=0x7f020015;
-        public static final int small=0x7f020016;
+        public static final int bus=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int icon=0x7f020007;
+        public static final int m1=0x7f020008;
+        public static final int m10=0x7f020009;
+        public static final int m2=0x7f02000a;
+        public static final int m3=0x7f02000b;
+        public static final int m4=0x7f02000c;
+        public static final int m5=0x7f02000d;
+        public static final int m6=0x7f02000e;
+        public static final int m7=0x7f02000f;
+        public static final int m8=0x7f020010;
+        public static final int m9=0x7f020011;
+        public static final int marker=0x7f020012;
+        public static final int marker2=0x7f020013;
+        public static final int mini=0x7f020014;
+        public static final int small=0x7f020015;
     }
     public static final class id {
         public static final int balloon_close=0x7f050001;
@@ -42,23 +41,27 @@ public final class R {
         public static final int balloon_item_snippet=0x7f050004;
         public static final int balloon_item_title=0x7f050003;
         public static final int balloon_main_layout=0x7f050002;
-        public static final int help=0x7f05000a;
+        public static final int help=0x7f05000b;
         public static final int mainlayout=0x7f050006;
         public static final int mapview=0x7f050007;
-        public static final int menu_search=0x7f050009;
+        public static final int menu_search=0x7f05000a;
+        public static final int search=0x7f050009;
         public static final int status_text=0x7f050008;
     }
     public static final class layout {
         public static final int balloon_overlay=0x7f030000;
         public static final int list_item=0x7f030001;
         public static final int main=0x7f030002;
+        public static final int search=0x7f030003;
     }
     public static final class menu {
         public static final int menu=0x7f070000;
     }
     public static final class string {
+        public static final int app_label=0x7f060003;
         public static final int app_name=0x7f060001;
         public static final int hello=0x7f060000;
+        public static final int search_hint=0x7f060002;
     }
     public static final class xml {
         public static final int searchable=0x7f040000;
